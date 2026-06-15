2fcd42964e5ae1748c748143d97e773dac682100
Sourabh Yadav <sourabhyadav626333@gmail.com>
2026-06-16 02:20:04 +0530

    server connected

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listen.js");
 
-const url = 'mongodb://127.0.0.1:27017/test';
+const url = 'mongodb://127.0.0.1:27017/wonderlust';
 
 main()
     .then (()=> {
@@ -17,7 +18,21 @@ async function main() {
 
 app.get("/", (req,res)=> {
     res.send("Hello");
-})
+});
+ 
+app.get("/testlisting", async (req,res)=>{
+    let sample = new Listing({
+        title : "My new villa",
+        discription : "By the beach",
+        price : 2000,
+        location : "goa",
+        country : "India",
+    })
+
+    await sample.save();
+    console.log("response send");
+    res.send("Successful");
+});
 
 app.listen(8080, ()=>{
     console.log("Connected");
---
+++ models/listen.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title : {
+        type : String,
+        required : true,
+    },
+    discription : String,
+    image : {
+        type : String,
+        default : "https://imgs.search.brave.com/vaWDz9DEX7iLcz6JG9bm5I_2WwjGve2X-ydUASr6800/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMucGV4ZWxzLmNv/bS9waG90b3MvMjc1/NjYzNzQvcGV4ZWxz/LXBob3RvLTI3NTY2/Mzc0L2ZyZWUtcGhv/dG8tb2YtdmFya2Fs/YS1iZWFjaC1jbGlm/ZnNpZGUuanBlZz9h/dXRvPWNvbXByZXNz/JmNzPXRpbnlzcmdi/JmRwcj0xJnc9NTAw",
+        set : (v) => v === "" ? "https://imgs.search.brave.com/vaWDz9DEX7iLcz6JG9bm5I_2WwjGve2X-ydUASr6800/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMucGV4ZWxzLmNv/bS9waG90b3MvMjc1/NjYzNzQvcGV4ZWxz/LXBob3RvLTI3NTY2/Mzc0L2ZyZWUtcGhv/dG8tb2YtdmFya2Fs/YS1iZWFjaC1jbGlm/ZnNpZGUuanBlZz9h/dXRvPWNvbXByZXNz/JmNzPXRpbnlzcmdi/JmRwcj0xJnc9NTAw"
+        : v,
+    },
+    price : Number,
+    location : String,
+    country : String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
