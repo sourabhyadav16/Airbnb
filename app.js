0d7bebc0d4498780da4a58a4e0eccd35f716e088
Sourabh Yadav <sourabhyadav626333@gmail.com>
2026-06-23 03:43:08 +0530

    Added basic functionalities

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listen.js");
 const path = require("path");
+const methodOverride = require("method-override"); //for put method
 
 const url = 'mongodb://127.0.0.1:27017/wonderlust';
 
@@ -20,7 +21,9 @@ async function main() {
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended : true})); //to parse the data
+app.use(methodOverride("_method"));
 
+//Main root
 app.get("/", (req,res)=> {
     res.send("Hello");
 });
@@ -31,13 +34,45 @@ app.get("/listings", async (req,res)=> {
     res.render("listings/index.ejs", {allListings});
 });
 
+//Create new
+app.get("/listings/new", (req,res)=>{
+    res.render("listings/new.ejs");
+});
+
 //Show route
 app.get("/listings/:id", async (req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
-    console.log(listing);
     res.render("listings/show.ejs", {listing});
-})
+});
+
+//New Post
+app.post("/listings", async(req,res)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+//Get update
+app.get("/listings/:id/edit", async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
+
+//Put update
+app.put("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);    
+});
+
+//Delete route
+app.delete("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    let deleteListing = await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+});
 
 app.listen(8080, ()=>{
     console.log("Connected");
